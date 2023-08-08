ad5392eaa95db244377b392dac4f5c3411c53ac1
Gary Hung <gary.hung@ninesmart.io>
2023-08-08 15:47:01 +0800

    Add webpack and tsconfig

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+console.log("Our mini webpack-ts project");
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const webpack = require("webpack");
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+  target: "node",
+  externals: [nodeExternals()],
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    library: "mini-webpack-ts-project",
+    libraryTarget: "umd",
+  },
+  mode: "production",
+  entry: {
+    main: path.resolve(__dirname, "src/index.ts"),
+  },
+  devtool: false,
+  module: {
+    rules: [
+      {
+        test: /\.ts$/i,
+        loader: "ts-loader",
+      },
+    ],
+  },
+};
